77ac561db535cb41c4aa580878cc7db7e33c2d49
mirokshi <rojasdiazmirokshi@gmail.com>
2018-10-10 21:16:22 +0200

    WIP

--- Lambda/src/lambda/Lambda.java
+++ Lambda/src/lambda/Lambda.java
@@ -10,53 +10,43 @@ package lambda;
  * @author mirokshi
  */
 public class Lambda {
+        interface OperacionMatematica {
+
+        int operar(int a, int b);
+    }
+        
+
+    public static int operar(int a, int b, OperacionMatematica OperacionMatematica) {
+        return OperacionMatematica.operar(a, b);
+    }
+
 
     public static void main(String args[]) {
-        Lambda tester = new Lambda();
+
 
         //with type declaration
-        MathOperation addition = (int a, int b) -> a + b;
+        OperacionMatematica suma = (int a, int b) -> a + b;
 
-        //with out type declaration
-        MathOperation subtraction = (a, b) -> a - b;
+//        //with out type declaration
+//        OperacionMatematica resta = (a, b) -> a - b;
+//
+//        //with return statement along with curly braces
+//        OperacionMatematica multiplicacion = (int a, int b) -> {
+//            return a * b;
+//        };
+//
+//        //without return statement and without curly braces
+//        OperacionMatematica division = (int a, int b) -> a / b;
 
-        //with return statement along with curly braces
-        MathOperation multiplication = (int a, int b) -> {
-            return a * b;
-        };
+        System.out.println("10 + 5 = " + operar(10, 5, suma));
+        
+//        System.out.println("10 - 5 = " + tester.operar(10, 5, resta));
+//        System.out.println("10 x 5 = " + tester.operar(10, 5, multiplicacion));
+//        System.out.println("10 / 5 = " + tester.operar(10, 5, division));
 
-        //without return statement and without curly braces
-        MathOperation division = (int a, int b) -> a / b;
-
-        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
-        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
-        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
-        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
-
-        //without parenthesis
-        GreetingService greetService1 = message
-                -> System.out.println("Hello " + message);
-
-        //with parenthesis
-        GreetingService greetService2 = (message)
-                -> System.out.println("Hello " + message);
-
-        greetService1.sayMessage("Mahesh");
-        greetService2.sayMessage("Suresh");
+        
     }
 
-    interface MathOperation {
 
-        int operation(int a, int b);
-    }
-
-    interface GreetingService {
-
-        void sayMessage(String message);
-    }
-
-    private int operate(int a, int b, MathOperation mathOperation) {
-        return mathOperation.operation(a, b);
-    }
 }
         
