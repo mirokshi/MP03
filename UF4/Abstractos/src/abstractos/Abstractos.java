988b6f6147131ef4ce08bf12017f6b0b8293050e
mirokshi <rojasdiazmirokshi@gmail.com>
2018-10-25 16:22:06 +0200

    WIP

--- UF4/Abstractos/src/abstractos/Abstractos.java
+++ UF4/Abstractos/src/abstractos/Abstractos.java
@@ -1,37 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abstractos;
 
-/**
- *
- * @author mirokshi
- */
 public class Abstractos {
 
     public static void main(String arg[]) {
-        /**
-         * Objeto miGuitarra de tipo Instrumento
-         */
         Instrumento miGuitarra = new Guitarra();
         System.out.println("Instrumento : " + miGuitarra.tipo);
         miGuitarra.tocar();
         System.out.println();
-        /**
-         * Objeto miSaxofon de tipo Instrumento
-         */
         Instrumento miSaxofon = new Saxofon();
         System.out.println("Instrumento : " + miSaxofon.tipo);
         miSaxofon.tocar();
         System.out.println();
-        /**
-         * Objeto miViolin de tipo Instrumento
-         */
         Instrumento miViolin = new Violin();
         System.out.println("Instrumento : " + miViolin.tipo);
         miViolin.tocar();
-
     }
 }
--- UF4/Abstractos/src/abstractos/Instrumento.java
+++ UF4/Abstractos/src/abstractos/Instrumento.java
@@ -1,71 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abstractos;
 
-/**
- *
- * @author mirokshi
- */
-
-/**
- * Clase Abstracta Instrumento
- * La cual posee una propiedad tipo y un método abstracto tocar()
- * Todas las clases hijas que tenga están obligadas a implementar este método(tocar())
- */
-abstract class Instrumento { 
+public abstract class Instrumento {
 
     public String tipo;
 
-    abstract void tocar(); //Metodo abstracto 
+    abstract void tocar();
 }
 
-/**
- * Clase Concreta Guitarra, hija de Instrumento
- */
 class Guitarra extends Instrumento {
 
     public Guitarra() {
         tipo = "Guitarra";
     }
 
-    public  void tocar() { //hereda este metodo de la clase padre 
+    public void tocar() {
         System.out.println("Tocar La Guitarra");
     }
 }
 
-/**
- * Clase Concreta Violin, hija de Instrumento
- */
 class Violin extends Instrumento {
 
     public Violin() {
         tipo = "Violin";
     }
 
-    public void tocar() { //hereda este metodo de la clase padre 
+    public void tocar() {
         System.out.println("Tocar El violin");
     }
 }
 
-/**
- * Clase Concreta Saxofon, hija de Instrumento
- */
 class Saxofon extends Instrumento {
 
     public Saxofon() {
         tipo = "Saxofon";
     }
 
-    public void tocar() { //hereda este metodo de la clase padre 
+    public void tocar() {
         System.out.println("Tocar el Saxofon");
     }
-    
-    /**
-     * Cada una de las clases concretas implementan el método tocar()
-     * En cada constructor de las clases definimos el tipo
-     * En las clases concretas no tenemos la variable tipo declarada pues se usa la variable heredada de la clase Instrumento.
-     */
 }
