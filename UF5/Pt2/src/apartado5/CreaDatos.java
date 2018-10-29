2dbb0dd677dd22a34cfc0492b8b39c5502d2ffb6
mirokshi <rojasdiazmirokshi@gmail.com>
2018-10-29 19:32:02 +0100

    WIP

--- UF5/Pt2/src/apartado5/CreaDatos.java
+++ UF5/Pt2/src/apartado5/CreaDatos.java
@@ -5,10 +5,42 @@
  */
 package apartado5;
 
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  *
  * @author mirokshi
  */
 public class CreaDatos {
+
+    static final String dataFile = "aleatorio";
+    static final int numero =142;
     
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        DataOutputStream out = null;
+        
+
+        try {
+            out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(dataFile)));
+
+            for (int i = 0; i < numero; i++) {
+                    out.writeUTF(" ");
+                    out.writeInt(numero);
+
+            }
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+
+    }
 }
